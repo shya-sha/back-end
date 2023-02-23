da8df49ee8892f48f9b2e67fb25461d638348a6f
shya-sha <124905644+shya-sha@users.noreply.github.com>
2023-02-23 09:32:40 +0800

    Add files via upload

---
+++ Exercise #04/members.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id : 1,
+        nim : "105033008248",
+        name : "Anggreiny Deeng",
+        phoneNumber : "0823xxxxxx",
+    },
+    {
+        id : 2,
+        nim : "105022010033",
+        name : "Shyalenn Kolibonso",
+        phoneNumber : "0823xxxxxx",
+    },
+    {
+        id : 3,
+        nim : "105030294092",
+        name : "Vincentius Manibuy",
+        phoneNumber : "0823xxxxxx",
+    },
+    {
+        id : 4,
+        nim : "105030294025",
+        name : "Vannesa Kilis",
+        phoneNumber : "0823xxxxxx",
+    },
+];
